5d8ec0e35068241b960f42eddbd202340fb9ddb4
tawsifhye <tawsif.lucse@gmail.com>
2021-10-11 12:37:42 +0600

    Create login, register component and set route

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD5BzBW-tjFnUagQSRGCFEEktHZOAj5yew",
+    authDomain: "ema-jhon-simple-1fd97.firebaseapp.com",
+    projectId: "ema-jhon-simple-1fd97",
+    storageBucket: "ema-jhon-simple-1fd97.appspot.com",
+    messagingSenderId: "940241772806",
+    appId: "1:940241772806:web:b5e77c03477fde360ffe87"
+  };
+  
+  export default firebaseConfig;
