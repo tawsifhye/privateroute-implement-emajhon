e2fcb2a524b6b5f291982acdd8a00b67c7fb36b8
tawsifhye <tawsif.lucse@gmail.com>
2021-10-12 01:52:43 +0600

    user context and set logged in user in context

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
+    const {user, logOut} = useFirebase();
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
@@ -11,7 +13,12 @@ const Header = () => {
                 <NavLink to="/shop">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
                 <NavLink to="/inventory">Manage Inventory</NavLink>
-                <NavLink to="/login">Login</NavLink>
+                {
+                    user.email ? 
+                        <button onClick={logOut}>Log Out</button>
+                        :
+                        <NavLink to="/login">Login</NavLink>
+                }
             </nav>
         </div>
     );
